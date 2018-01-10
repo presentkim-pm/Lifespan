b8b6cece6ba9e69ee2855c9b3291f1a467890061
PresentKim <qjrmckd@gmail.com>
2018-01-10 20:52:48 +0900

    Add main class

---
+++ src/presentkim/lifespan/LifeSpanMain.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace presentkim\lifespan;
+
+use pocketmine\command\{
+  CommandExecutor, PluginCommand
+};
+use pocketmine\plugin\PluginBase;
+use presentkim\lifespan\{
+  listener\EntityEventListener, command\CommandListener, util\Translation
+};
+
+class LifeSpanMain extends PluginBase{
+
+    /** @var self */
+    private static $instance = null;
+
+    /** @var PluginCommand[] */
+    private $commands = [];
+
+    /** @return self */
+    public static function getInstance(){
+        return self::$instance;
+    }
+
+    public function onLoad(){
+        if (self::$instance === null) {
+            // register instance
+            self::$instance = $this;
+
+            // load utils
+            $this->getServer()->getLoader()->loadClass('presentkim\lifespan\util\Utils');
+        }
+    }
+
+    public function onEnable(){
+        $this->load();
+
+        // register event listeners
+        $this->getServer()->getPluginManager()->registerEvents(new EntityEventListener(), $this);
+    }
+
+    public function load(){
+        $dataFolder = $this->getDataFolder();
+        if (!file_exists($dataFolder)) {
+            mkdir($dataFolder, 0777, true);
+        }
+
+        // load db
+        $this->saveDefaultConfig();
+        $this->reloadConfig();
+
+        // load lang
+        $langfilename = $dataFolder . 'lang.yml';
+        if (!file_exists($langfilename)) {
+            Translation::loadFromResource($this->getResource('lang/eng.yml'));
+            Translation::save($langfilename);
+        } else {
+            Translation::load($langfilename);
+        }
+
+        // unregister commands
+        foreach ($this->commands as $command) {
+            $this->getServer()->getCommandMap()->unregister($command);
+        }
+        $this->commands = [];
+
+        // register commands
+        $this->registerCommand(new CommandListener($this), Translation::translate('command-lifespan'), 'LifeSpan', 'lifespan.cmd', Translation::translate('command-lifespan@description'), Translation::translate('command-lifespan@usage'), Translation::getArray('command-lifespan@aliases'));
+    }
+
+    public function save(){
+        $dataFolder = $this->getDataFolder();
+        if (!file_exists($dataFolder)) {
+            mkdir($dataFolder, 0777, true);
+        }
+
+        // save db
+        $this->saveConfig();
+
+        // save lang
+        Translation::save($dataFolder . 'lang.yml');
+    }
+
+    /**
+     * @param CommandExecutor $executor
+     * @param                 $name
+     * @param                 $fallback
+     * @param                 $permission
+     * @param string          $description
+     * @param null            $usageMessage
+     * @param array|null      $aliases
+     */
+    private function registerCommand(CommandExecutor $executor, $name, $fallback, $permission, $description = "", $usageMessage = null, array $aliases = null){
+        $command = new PluginCommand($name, $this);
+        $command->setExecutor($executor);
+        $command->setPermission($permission);
+        $command->setDescription($description);
+        $command->setUsage($usageMessage ?? ('/' . $name));
+        if (is_array($aliases)) {
+            $command->setAliases($aliases);
+        }
+
+        $this->getServer()->getCommandMap()->register($fallback, $command);
+        $this->commands[] = $command;
+    }
+}
