5ef36af7f66e975ea515047f66b879f4fefdf0de
PresentKim <qjrmckd@gmail.com>
2018-01-16 22:40:40 +0900

    Fix save lang in load()

--- src/presentkim/lifespan/LifeSpanMain.php
+++ src/presentkim/lifespan/LifeSpanMain.php
@@ -60,8 +60,10 @@ class LifeSpanMain extends PluginBase{
         // load lang
         $langfilename = $dataFolder . 'lang.yml';
         if (!file_exists($langfilename)) {
-            Translation::loadFromResource($this->getResource('lang/eng.yml'));
-            Translation::save($langfilename);
+            $resource = $this->getResource('lang/eng.yml');
+            Translation::loadFromResource($resource);
+            stream_copy_to_stream($resource, $fp = fopen("{$dataFolder}lang.yml", "wb"));
+            fclose($fp);
         } else {
             Translation::load($langfilename);
         }
