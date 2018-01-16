ffb6012ebd76dba2602697aa2dd1a1e2058f70bc
PresentKim <qjrmckd@gmail.com>
2018-01-16 22:18:58 +0900

    Fix translate and getArray in Translation

--- src/presentkim/lifespan/util/Translation.php
+++ src/presentkim/lifespan/util/Translation.php
@@ -2,6 +2,8 @@
 
 namespace presentkim\lifespan\util;
 
+use presentkim\lifespan\LifeSpanMain as Plugin;
+
 class Translation{
 
     /** @var string[string] */
@@ -59,16 +61,22 @@ class Translation{
     public static function translate(string $strId, string ...$params){
         if (isset(self::$lang[$strId])) {
             $value = self::$lang[$strId];
-            if (is_array($value)) {
-                $value = $value[array_rand($value)];
-            }
-            if (is_string($value)) {
-                return empty($params) ? $value : strtr($value, listToPairs($params));
-            } else {
-                return "$strId is not string";
-            }
+        } elseif (isset(self::$default[$strId])) {
+            Plugin::getInstance()->getLogger()->warning("get $strId from default");
+            $value = self::$default[$strId];
+        } else {
+            Plugin::getInstance()->getLogger()->warning("get $strId failed");
+            return "Undefined strId : $strId";
         }
-        return "Undefined \$strId : $strId";
+
+        if (is_array($value)) {
+            $value = $value[array_rand($value)];
+        }
+        if (is_string($value)) {
+            return empty($params) ? $value : strtr($value, listToPairs($params));
+        } else {
+            return "$strId is not string";
+        }
     }
 
     /**
@@ -79,8 +87,13 @@ class Translation{
     public static function getArray(string $strId){
         if (isset(self::$lang[$strId])) {
             $value = self::$lang[$strId];
-            return is_array($value) ? $value : null;
+        } elseif (isset(self::$default[$strId])) {
+            Plugin::getInstance()->getLogger()->warning("get $strId from default");
+            $value = self::$default[$strId];
+        } else {
+            Plugin::getInstance()->getLogger()->warning("get $strId failed");
+            return null;
         }
-        return null;
+        return is_array($value) ? $value : null;
     }
 }
