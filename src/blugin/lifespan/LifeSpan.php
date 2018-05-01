4c8cc6675e855b3d2b36621a8a9d2bd1c4fdeb0d
PresentKim <qjrmckd@gmail.com>
2018-05-01 17:57:56 +0900

    Add declare : strict_types=1

--- src/blugin/lifespan/LifeSpan.php
+++ src/blugin/lifespan/LifeSpan.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace blugin\lifespan;
 
 use pocketmine\command\{
--- src/blugin/lifespan/listener/EntityEventListener.php
+++ src/blugin/lifespan/listener/EntityEventListener.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace blugin\lifespan\listener;
 
 use pocketmine\entity\Entity;
