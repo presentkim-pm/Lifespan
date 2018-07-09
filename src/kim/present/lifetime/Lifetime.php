18d031b46d3d261fb1bcb984e189f6c6963785f0
PresentKim <qjrmckd@gmail.com>
2018-07-09 21:08:23 +0900

    Revert "Change visibility of onLoad() and onEnable() to protected"
    This reverts commit 9ac9faa7173d3c85de087ca0156fff6f7b40b3bd.

--- src/kim/present/lifetime/Lifetime.php
+++ src/kim/present/lifetime/Lifetime.php
@@ -37,14 +37,14 @@ class Lifetime extends PluginBase implements CommandExecutor{
 	/**
 	 * Called when the plugin is loaded, before calling onEnable()
 	 */
-	protected function onLoad() : void{
+	public function onLoad() : void{
 		self::$instance = $this;
 	}
 
 	/**
 	 * Called when the plugin is enabled
 	 */
-	protected function onEnable() : void{
+	public function onEnable() : void{
 		$dataFolder = $this->getDataFolder();
 		if(!file_exists($dataFolder)){
 			mkdir($dataFolder, 0777, true);
@@ -77,6 +77,10 @@ class Lifetime extends PluginBase implements CommandExecutor{
 		$this->getServer()->getPluginManager()->registerEvents(new EntityEventListener($this), $this);
 	}
 
+	/**
+	 * Called when the plugin is disabled
+	 * Use this to free open things and finish actions
+	 */
 	public function onDisable(){
 		$dataFolder = $this->getDataFolder();
 		if(!file_exists($dataFolder)){
