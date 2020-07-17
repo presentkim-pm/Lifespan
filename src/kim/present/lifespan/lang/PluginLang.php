36a442d589aae9c6546772aca18dd4b787391d2a
PresentKim <debe3721@gmail.com>
2020-07-17 13:27:56 +0900

    Change license to LGPL-3.0

--- src/kim/present/lifespan/lang/PluginLang.php
+++ src/kim/present/lifespan/lang/PluginLang.php
@@ -9,11 +9,13 @@
  * |_|   |_|  \___||___/\___|_| |_|\__|_|\_\_|_| |_| |_|
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the MIT License. see <https://opensource.org/licenses/MIT>.
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * @author  PresentKim (debe3721@gmail.com)
  * @link    https://github.com/PresentKim
- * @license https://opensource.org/licenses/MIT MIT License
+ * @license https://www.gnu.org/licenses/lgpl-3.0 LGPL-3.0 License
  *
  *   (\ /)
  *  ( . .) ♥
